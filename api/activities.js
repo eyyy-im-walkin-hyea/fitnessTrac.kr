494f9fbad736b5e96f802419194b1e7cac6605db
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-03-23 19:35:52 -0600

    Updating API Activities

--- api/activities.js
+++ api/activities.js
@@ -3,22 +3,22 @@ const router = express.Router();
 
 // GET /api/activities/:activityId/routines
 router.get("/:activityId/routines", async (req, res, next) => {
-
+// Get a list of all public routines which feature that activity
 });
 
 // GET /api/activities
 router.get("/", async (req, res, next) => {
-
+// Just return a list of all activities in the database
 });
 
 // POST /api/activities
 router.post("/", async (req, res, next) => {
-
+// Create a new activity
 });
 
 // PATCH /api/activities/:activityId
 router.patch("/:activityId", async (req, res, next) => {
-
-})
+// Anyone can update an activity (yes, this could lead to long term problems a la wikipedia)
+});
 
 module.exports = router;
