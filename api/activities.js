a5d230834204cda6a85c9fefd1b2044d7c154244
Bryan Runnels <bryan.d.runnels@gmail.com>
2023-03-23 19:26:39 -0600

    Working on API Activities

--- api/activities.js
+++ api/activities.js
@@ -2,11 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 // GET /api/activities/:activityId/routines
+router.get("/:activityId/routines", async (req, res, next) => {
+
+});
 
 // GET /api/activities
+router.get("/", async (req, res, next) => {
+
+});
 
 // POST /api/activities
+router.post("/", async (req, res, next) => {
+
+});
 
 // PATCH /api/activities/:activityId
+router.patch("/:activityId", async (req, res, next) => {
+
+})
 
 module.exports = router;
