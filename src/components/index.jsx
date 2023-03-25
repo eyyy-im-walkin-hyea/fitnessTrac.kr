5e8718088e95ff428cab0943c071aecee7b65438
David Burkhart <dburkhart77@gmail.com>
2023-03-25 11:35:05 -0600

    mod index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,5 @@ export { default as Activities } from './activities';
 export { default as Homepage } from './homepage';
 export { default as LogIn } from './login';
 export { default as MyRoutines } from './myRoutines';
-export { default as Navbar } from './navbar';
 export { default as Registration } from './registration';
 export { default as Routines } from './routines';
